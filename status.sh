68a531765ee6bb7b8485987b6824e5ea172c8cf8
dev12rathore <dev12rathore@gmail.com>
2017-06-26 18:57:58 +0530

    e

--- status.sh
+++ status.sh
@@ -11,7 +11,16 @@ fi
 
 if [ -z "$1" ]
   then
-    echo "No argument supplied"
+    echo "No VERSION Specify"
 else
-    echo "$1" 
+    echo "$1"
+    VERSION=${1}
+
+    docker build \
+      --build-arg version=${VERSION} \
+      --build-arg gitbranch=$(git rev-parse --abbrev-ref HEAD) \
+      --build-arg githash=$(git rev-parse --short HEAD) \
+      -t cavo2/testing{VERSION} -f worker-ui/Dockerfile . 
+    git tag ${VERSION}
+    git push --tags
 fi
