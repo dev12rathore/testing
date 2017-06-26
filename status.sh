0e71c84e87d60de3ffd964cb53bfcefefafa0066
dev12rathore <dev12rathore@gmail.com>
2017-06-26 18:46:42 +0530

    1

--- status.sh
+++ status.sh
@@ -2,9 +2,12 @@ git diff --no-ext-diff --quiet --exit-code
 STATUS=$?
 if [ $STATUS -eq 0 ];
   then
-        echo -e "Database '$DBNAME' is created"
+        echo -e "Database is updated"
 
 else
-       echo -e "Failed to create database '$DBNAME'"
+       echo -e "Please commit all cheanges first"
+       exit 0
 fi
+
+ls -l
 
