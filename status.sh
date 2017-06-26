ac90eeeaec3a92fad0804bf7c21d34804f5ca1ef
dev12rathore <dev12rathore@gmail.com>
2017-06-26 19:37:40 +0530

    6

--- status.sh
+++ status.sh
@@ -1,27 +1,42 @@
+#!/bin/bash
+USER=cavo2
+
 git diff --no-ext-diff --quiet --exit-code
 STATUS=$?
+
 if [ $STATUS -eq 0 ];
   then
-        echo -e "Database is updated"
-
+     echo "Git is Look good"
 else
-       echo -e "Please commit all cheanges first"
+       echo -e "Please commit all changes first"
        exit 0
 fi
 
 if [ -z "$1" ]
   then
     echo "No VERSION Specify"
+    exit 0
 else
-    echo "$1"
+
     VERSION=${1}
 
-docker build \
---build-arg version=${VERSION} \
---build-arg gitbranch=$(git rev-parse --abbrev-ref HEAD) \
---build-arg githash=$(git rev-parse --short HEAD) \
--t cavo2/testing:${VERSION} -f worker-ui/Dockerfile . 
-    
-git tag ${VERSION}
-git push --tags
+    git tag ${VERSION}
+    git push --tags
+
 fi
+
+    docker build \
+      --build-arg version=${VERSION} \
+      --build-arg gitbranch=$(git rev-parse --abbrev-ref HEAD) \
+      --build-arg githash=$(git rev-parse --short HEAD) \
+      -t $USER/testing:${VERSION} -f worker-ui/Dockerfile .
+   
+     SaTATUS=$?
+     if [ $SaTATUS -eq 0 ];
+       then
+          docker login -u $USER
+          docker push $USER/testing:${VERSION}
+     else
+          echo -e “docker images is not build \n Please find error“
+          exit 0
+     fi
